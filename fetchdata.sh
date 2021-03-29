e84614e634ca65a7e136bc21ac31cae3ba71a663
Er Jun Li <er.li@mail.mcgill.ca>
2021-03-28 23:14:34 -0400

    new fetch data

--- fetchdata.sh
+++ fetchdata.sh
@@ -6,11 +6,15 @@ wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
 # answers
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 
-# balanced pairs
+# complementary pair list
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Complementary_Pairs_Train_mscoco.zip https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Complementary_Pairs_Val_mscoco.zip
 
-# bottom up features (https://github.com/peteanderson80/bottom-up-attention)
-wget https://imagecaption.blob.core.windows.net/imagecaption/trainval.zip https://imagecaption.blob.core.windows.net/imagecaption/test2015.zip
+# VQA input images
+wget http://images.cocodataset.org/zips/train2014.zip http://images.cocodataset.org/zips/val2014.zip http://images.cocodataset.org/zips/test2015.zip
+
+# image caption
+# wget https://imagecaption.blob.core.windows.net/imagecaption/trainval.zip https://imagecaption.blob.core.windows.net/imagecaption/test2015.zip
+
 ## alternative bottom-up features: 36 fixed proposals per image instead of 10--100 adaptive proposals per image.
 #wget https://imagecaption.blob.core.windows.net/imagecaption/trainval_36.zip https://imagecaption.blob.core.windows.net/imagecaption/test2015_36.zip
 
