3c2a4e1b2e4ab7a6783f5d6f018179135ea23645
Er Jun Li <er.li@mail.mcgill.ca>
2021-04-04 14:19:02 -0400

    update fetchdata script

--- fetchdata.sh
+++ fetchdata.sh
@@ -3,7 +3,7 @@
 # questions
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Test_mscoco.zip
 
-# answers
+# annotations
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 
 # complementary pair list
@@ -12,10 +12,14 @@ wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Complementary_Pairs_Train_
 # VQA input images
 wget http://images.cocodataset.org/zips/train2014.zip http://images.cocodataset.org/zips/val2014.zip http://images.cocodataset.org/zips/test2015.zip
 
-# image caption
-# wget https://imagecaption.blob.core.windows.net/imagecaption/trainval.zip https://imagecaption.blob.core.windows.net/imagecaption/test2015.zip
+# create directories for data
+mkdir Annotations
+mkdir "Complementary Pairs"
+mkdir Questions
+mkdir Images
 
-## alternative bottom-up features: 36 fixed proposals per image instead of 10--100 adaptive proposals per image.
-#wget https://imagecaption.blob.core.windows.net/imagecaption/trainval_36.zip https://imagecaption.blob.core.windows.net/imagecaption/test2015_36.zip
-
-unzip "*.zip"
+# unzip images
+unzip train2014.zip val2014.zip test2015.zip -d Images
+unzip v2_Annotations_Train_mscoco.zip v2_Annotations_Val_mscoco.zip -d Annotations
+unzip v2_Complementary_Pairs_Train_mscoco.zip v2_Complementary_Pairs_Val_mscoco.zip -d "Complementary Pairs"
+unzip v2_Questions_Train_mscoco.zip v2_Questions_Val_mscoco.zip v2_Questions_Test_mscoco.zip -d Questions
