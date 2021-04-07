63a7364d91270d56442f4268f44e05fd2dd9948b
Er Jun Li <er.li@mail.mcgill.ca>
2021-04-07 15:47:55 -0400

    template to train vqa model

---
+++ train.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+from datetime import datetime
+import os
+import json
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning import loggers 
+
+import torch
+from torch.utils.data import DataLoader
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def train(model, train_dataloader: DataLoader, val_dataloader:DataLoader, epochs: int, patience:int = 5):
+    # create folder for each run
+    folder = "models/{}".format(datetime.now().strftime("%b-%d-%H-%M-%S"))
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # early stoppping
+    early_stopping_callback = EarlyStopping(
+      monitor='val_loss', # monitor validation loss
+      verbose=True, # log early-stop events
+      patience=patience,
+      min_delta=0.00 # minimum change is 0
+      )
+
+    # update checkpoints based on validation loss by using ModelCheckpoint callback monitoring 'val_loss'
+    checkpoint_callback = ModelCheckpoint(monitor='val_loss')
+
+    logger = loggers.CometLogger( 
+            save_dir="logs/",
+            workspace="vqa",
+            project_name=f"{type(model).__name__}", 
+            experiment_name=f"{type(model).__name__}_{datetime.now().strftime('%b_%d_%H_%M_%S')}"
+        )
+
+    # define trainer 
+    trainer = pl.Trainer(
+      default_root_dir=folder, # Lightning automates saving and loading checkpoints
+      max_epochs=epochs, gpus=0,
+      logger=logger, 
+      progress_bar_refresh_rate=30, 
+      callbacks=[early_stopping_callback, checkpoint_callback])
+
+    # train
+    trainer.fit(model=model, train_dataloader=train_dataloader, val_dataloaders=val_dataloader)
+
+    # save test result
+    PATH = folder + '/result'
+    with open(PATH, "w") as f:
+        f.write(f"Model: {str(model)}\n")
+        f.write(json.dumps(model.logger.metrics))
+        f.write("\n")
+        f.write(f"Lowest training loss: {str(min(model.logger.metrics['train_loss']))}\n")
+        f.write(f"Lowest validation loss: {str(min(model.metrics['val_loss']))}\n")
+
+    # plot training vs validation loss
+    plt.plot(range(len(model.logger.metrics['train_loss'])), model.logger.metrics['train_loss'], lw=2, label='Training Loss')
+    plt.plot(range(len(model.logger.metrics['val_loss'])), model.logger.metrics['val_loss'], lw=2, label='Validation Loss')
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.savefig(folder + f"/{type(model).__name__}_training_validation_loss.png")
+    plt.show()
