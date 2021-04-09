453246435351b688bff5fb825ebd8272d55c1a4f
William Zhang <w.ykzhang@hotmail.com>
2021-04-09 17:14:30 -0400

    added bash file for job for me

---
+++ run_will.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1              # Number of GPU(s) per node
+#SBATCH --cpus-per-task=8         # CPU cores/threads
+#SBATCH --mem=24G                 # memory
+#SBATCH --time=3-0                # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
+#SBATCH --job-name=vqa         
+#SBATCH --output=logs/%x-%j.out
+#SBATCH --mail-user=william.zhang2@mail.mcgill.ca
+#SBATCH --mail-type=ALL
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# confirm gpu available
+nvidia-smi
+
+# validate if a parameter is provided 
+if [ $# -ne 1 ] 
+then 
+    echo "Usage $0 <filename>"
+else
+    # run the command
+    source vqa-env/bin/activate
+    python $1
+fi
