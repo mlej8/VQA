6af875196a203df547ffde40149f8e157b1c9129
Michael Li <er.li@mail.mcgill.ca>
2021-04-17 19:54:05 -0400

    Kept get_dataloader call

--- run_cheng.sh
+++ run_cheng.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1              # Number of GPU(s) per node
+#SBATCH --gres=gpu:0              # Number of GPU(s) per node
 #SBATCH --cpus-per-task=8         # CPU cores/threads
-#SBATCH --mem=24G                 # memory
-#SBATCH --time=3-0                # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
+#SBATCH --mem=10G                 # memory
+#SBATCH --time=1-0                # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
 #SBATCH --job-name=vqa         
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --mail-user=cheng.chen2@mail.mcgill.ca
@@ -11,7 +11,7 @@
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 # confirm gpu available
-nvidia-smi
+# nvidia-smi
 
 # validate if a parameter is provided 
 if [ $# -ne 1 ] 
