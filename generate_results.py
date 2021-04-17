9f2c516749b1201f3eb0061d88c4f1ccef3afe21
Michael Li <er.li@mail.mcgill.ca>
2021-04-17 00:15:45 -0400

    update generate_results

--- generate_results.py
+++ generate_results.py
@@ -18,21 +18,37 @@ from vqa_cnn_lstm import preprocess as original_preprocess
 
 # test VQA dataset
 from vqa import VQA, VQATest
-
+import logging
+logger = logging.getLogger(__name__)
 if __name__ == '__main__':
-    MODEL_PATH = 'models/OriginalVQA/4a618c13c47a4fd3955355bfd9546c41/checkpoints/epoch=2-step=41603.ckpt'
+    MODEL_PATH = '/home/mlej8/projects/def-armanfn/mlej8/VQA/models/OriginalVQA/4a618c13c47a4fd3955355bfd9546c41/checkpoints/epoch=2-step=41603.ckpt'
 
     # wether evaluation on test-dev or test-standard
     test_dev = False
     results = []
-
+    """ 
+    
+    """
     # create test dataset
+    testdev_dataset = VQATest(
+        testdev_quesFile,
+        test_imgDir,
+        transform=original_preprocess
+    )
+
     test_dataset = VQATest(
-        testdev_quesFile if test_dev else test_quesFile,
+        test_quesFile,
         test_imgDir,
         transform=original_preprocess
     )
     
+    testdev_loader = DataLoader(
+        testdev_dataset,
+        batch_size=32,
+        shuffle=False,
+        num_workers=0,
+    )
+
     test_loader = DataLoader(
         test_dataset,
         batch_size=32,
@@ -44,20 +60,29 @@ if __name__ == '__main__':
     model = OriginalVQA.load_from_checkpoint(checkpoint_path=MODEL_PATH)
     
     # determine dataSubType
-    dataSubType = testdev_dataSubType if test_dev else test_dataSubType
-    resultFile = os.path.join("Results", f"{type(model).__name__}_{taskType}_{dataType}_{dataSubType}_results.json")
+    resultFile = os.path.join("Results", f"{type(model).__name__}_{taskType}_{dataType}_results.json")
 
     # freeze all layers of the model and set it to evaluation mode
     model.freeze()
-    total_batches = len(test_loader)
+
+    logger.info(f"Predicting on test standard")
     for idx, batch in enumerate(test_loader):
-        if idx % 50:
-            print(f"Done batch number {idx} out of {total_batches}")
+        logger.info(f"Done batch {idx}")
         output = model(batch["image"], batch["question"])
         _, preds = torch.max(output, dim=1)
         for pred, q_id in zip(preds, batch["question_id"].tolist()):
             answer = VQA.answers_vocabulary.idx2word(pred)
             results.append({"question_id": q_id , "answer": answer})
-
+    logger.info(f"Done on test standard")
+    logger.info(f"Predicting on test dev")
+    for idx, batch in enumerate(testdev_loader):
+        logger.info(f"Done batch {idx}")
+        output = model(batch["image"], batch["question"])
+        _, preds = torch.max(output, dim=1)
+        for pred, q_id in zip(preds, batch["question_id"].tolist()):
+            answer = VQA.answers_vocabulary.idx2word(pred)
+            results.append({"question_id": q_id , "answer": answer})
+    logger.info(f"Done on test dev")
+    
     with open(resultFile, 'w') as outfile:
         json.dump(results, outfile)
