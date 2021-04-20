c10f20b6b97ea1621a5e165d52f966306f40f05f
Michael Li <er.li@mail.mcgill.ca>
2021-04-20 16:55:27 -0400

    Run settings

--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --gres=gpu:0              # Number of GPU(s) per node
+#SBATCH --gres=gpu:1              # Number of GPU(s) per node
 #SBATCH --cpus-per-task=8         # CPU cores/threads
-#SBATCH --mem=16G                 # memory
-#SBATCH --time=1-0                # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
-#SBATCH --job-name=vqa         
-#SBATCH --output=logs/%j.out
+#SBATCH --mem=24G                 # memory
+#SBATCH --time=2-0                # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
+#SBATCH --job-name=vqa-generate-results         
+#SBATCH --output=logs/test-%j.out
 #SBATCH --mail-user=er.li@mail.mcgill.ca
 #SBATCH --mail-type=ALL
 
